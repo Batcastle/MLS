aaf2276582116705b6e721e9dbc12f41d018453f
Thomas Castleman <31315409+Batcastle@users.noreply.github.com>
2018-09-12 08:55:41 -0400

    Update loader.sh

--- MLS/loader.sh
+++ MLS/loader.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
 #
-#  loader.sh
+# loader.sh
+#  
+#  Copyright 2018 Thomas Castleman <draugeros@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
 #   The following code runs 3 commands simultaneously:
 #       1st - starting the loading screen
 #       2nd - it sets Mycroft to start up in 3 seconds, to allow the loading screen time to work so they can be working at the same time
@@ -9,3 +28,5 @@
 ( python $HOME/mycroft-core/MLS/loader.py &
   sleep 3s && $HOME/mycroft-core/start-mycroft.sh all &
   sleep 8s && killall loader.py )
+  
+  ##EOF##
